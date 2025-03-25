5a5fb0b27745912a17fe553dab3b1c63aac400af
Muhammad Ata <muhammad.ata.operatingpro@gmail.com>
2025-03-25 16:52:03 +0500

    feat: added model object and todo resolver

---
+++ src/todo/dto/update-todo.input.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { InputType, Field } from '@nestjs/graphql';
+
+@InputType()
+export class UpdateTodoInput {
+  @Field({ nullable: true })
+  title?: string;
+
+  @Field({ nullable: true })
+  description?: string;
+
+  @Field({ nullable: true })
+  completed?: boolean;
+}
