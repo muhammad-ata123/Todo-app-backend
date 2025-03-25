3a022c2627a534216b0fcad1e299bf31bdc9a9d6
Muhammad Ata <muhammad.ata.operatingpro@gmail.com>
2025-03-26 00:46:43 +0500

    feat: setting config and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,20 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath: '.env',
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
-      
+
     }),
     TodoModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
